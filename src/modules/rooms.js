ac6d1a9616c4754361edf42113fc464f10dcbc4b
Stephanie Fitzgerald <s.e.v.fitzgerald@gmail.com>
2017-08-18 22:29:52 -0700

    Tested alternative impementation of random item placement

--- src/modules/rooms.js
+++ src/modules/rooms.js
@@ -1,90 +1,102 @@
-import gameItems from './items'
+import gameItems from './items';
 
 const playground = {
   key: 'playground',
   name: 'Playground',
   items: [],
   doors: ['gym']
-}
+};
 
 const cafeteria = {
   key: 'cafeteria',
   name: 'Food Trough',
   items: [],
   doors: ['eastHall', 'westHall', 'gym']
-}
+};
 
 const gym = {
   key: 'gym',
   name: 'Gym',
   items: [],
   doors: ['eastHall', 'cafeteria', 'playground']
-}
+};
 
 const eastHall = {
   key: 'eastHall',
   name: 'East Hall',
   items: [],
   doors: ['gym', 'office', 'cafeteria', 'westHall','artRoom']
-}
+};
 
 const westHall = {
   key: 'westHall',
   name: 'West Hall',
   items: [],
   doors: ['cafeteria', 'mathRoom', 'eastHall']
-}
+};
 
 const mathRoom = {
   key: 'mathRoom',
   name: 'Algebra for Everyone',
   items: [],
   doors: ['westHall', 'artRoom']
-}
+};
 
 const artRoom = {
   key: 'artRoom',
   name: 'Painting for People',
   items: [],
   doors: ['mathRoom', 'eastHall']
-}
+};
 
 const office = {
   key: 'office',
   name: 'Principal\'s Office',
   items: [],
   doors: ['eastHall']
+};
 
-}
-
-const rooms = [westHall, office, cafeteria, gym, eastHall, artRoom, mathRoom, playground]
-let maxItm = 3
-let minItm = 1
-
-function propagateItems(room) {
-  let roomItems = gameItems
-  let numbOfItems = Math.random()* (maxItm-minItm) + minItm
-  
-  for(let i = 0; i <= numbOfItems; i++) {
-    let randomIndex = Math.random()* (roomItems.length-0)
-    roomItems.push(roomItems[randomIndex])
-    roomItems.splice(randomIndex,1);
-  }
-  console.log(roomItems)
-  return roomItems
-}
+const rooms = [westHall, office, cafeteria, gym, eastHall, artRoom, mathRoom, playground];
+const items = ['cell phone', 'hall pass', 'math homework', 'banana peel', 'toilet paper statue', 'incriminating photograph', 'greasy pizza box'];
 
 rooms.forEach(room => {
-  room.items = propagateItems(room)
   room.doors = room.doors.map(door => {
-      return rooms.find(r => r.key === door);
+    return rooms.find(r => r.key === door);
   });
-  return room
+});
+
+// TODO: add in max num items per room--if needed?
+items.forEach(item => {
+  let randomIndex = Math.floor((Math.random() * rooms.length));
+  rooms[randomIndex].items.push(item);
 });
 
 
-
 export default rooms;
+
+// let maxItm = 3;
+// let minItm = 1;
+
+// function propagateItems(room) {
+//   let roomItems = gameItems;
+//   let numbOfItems = Math.random()* (maxItm-minItm) + minItm;
+  
+//   for(let i = 0; i <= numbOfItems; i++) {
+//     let randomIndex = Math.random()* (roomItems.length-0);
+//     roomItems.push(roomItems[randomIndex]);
+//     roomItems.splice(randomIndex,1);
+//   }
+//   console.log(roomItems);
+//   return roomItems;
+// }
+
+// rooms.forEach(room => {
+//   room.items = propagateItems(room);
+//   room.doors = room.doors.map(door => {
+//     return rooms.find(r => r.key === door);
+//   });
+//   return room;
+// });
 
 // const rooms = [
 // {
