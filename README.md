db014fbfcb2e9bf654c0962a8ca91f5c50054b2a
Anwar Montasir <anwarmontasir@gmail.com>
2017-08-18 17:19:50 -0700

    begin creating rooms.js

--- README.md
+++ README.md
@@ -9,3 +9,61 @@ You are a character in a maze. In every room you will have choices of direction 
 In some rooms you will interact with other characters, and exchange objects.
 
 Your goal is to defeat the guardian of the maze and exit through the door behind him.
+
+## How the maze works (our notes)
+
+Maze: 16 rooms(directions, actions, items, opponents)
+
+Characters (enter name):
+- Directions:
+- North
+- South
+- East
+- West
+- Action
+- Pick up
+- Drop
+- Use
+Items:
+- Action (pick up, drop, use)
+
+Rooms:
+- number=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+- Directions(north, south, east, west)
+- Action(pick up, drop, use)
+
+All Rooms:
+- Directions
+- Action
+
+Room0:
+- Directions
+- Action
+- 2 items
+
+Room4:
+- Opponent
+- Directions
+- Action
+- 2 items
+
+Room6:
+- 1 item(unknown-->might help with Room15)
+- Directions
+- Action
+
+Room7:
+- Opponent
+- Directions
+- Action
+- 2 items
+
+Room13:
+- 1 item(door)
+- Directions
+- Action
+
+Room15:
+- Opponent
+- Directions
+- Action
---
+++ src/components/rooms.js
@@ -0,0 +1,101 @@
+// create our rooms
+
+const rooms = [
+    {
+        key: 0,
+        directions: ['right'],
+        items: [],
+        characters: []
+    },
+    {
+        key: 1,
+        directions: ['up', 'right', 'left'],
+        items: [],
+        characters: []
+    },
+    {
+        key: 2,
+        directions: ['right', 'left'],
+        items: [],
+        characters: []
+    },
+    {
+        key: 3,
+        directions: ['up', 'left'],
+        items: [],
+        characters: []
+    },
+    {
+        key: 4,
+        directions: ['up'],
+        items: [],
+        characters: []
+    },
+    {
+        key: 5,
+        directions: ['up', 'down'],
+        items: [],
+        characters: []
+    },
+    {
+        key: 6,
+        directions: ['up'],
+        items: [],
+        characters: []
+    },
+    {
+        key: 7,
+        directions: ['down'],
+        items: [],
+        characters: []
+    },
+    {
+        key: 8,
+        directions: ['up', 'right', 'down'],
+        items: [],
+        characters: []
+    },
+    {
+        key: 9,
+        directions: ['down', 'left'],
+        items: [],
+        characters: []
+    },
+    {
+        key: 10,
+        directions: ['up', 'right', 'down'],
+        items: [],
+        characters: []
+    },
+    {
+        key: 11,
+        directions: ['up', 'left'],
+        items: [],
+        characters: []
+    },
+    {
+        key: 12,
+        directions: ['right', 'down'],
+        items: [],
+        characters: []
+    },
+    {
+        key: 13,
+        directions: ['right', 'left'],
+        items: [],
+        characters: []
+    },
+    {
+        key: 14,
+        directions: ['down', 'left'],
+        items: [],
+        characters: []
+    },
+    {
+        key: 15,
+        directions: ['down'],
+        items: [],
+        characters: []
+    }
+];
+
