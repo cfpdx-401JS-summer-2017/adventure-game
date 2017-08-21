9c6a24989065430cb9308200f78131bb78e4c8b5
Wendy Beck <webeck@gmail.com>
2017-08-21 14:31:49 -0700

    setting principal movements

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
     this.state = {
       rooms: rooms,
       playerRoom: rooms[0],
-      princRoom: rooms[7],
+      princRoom: rooms[1],
       item: {
         key: '',
         name: ''
@@ -24,21 +24,29 @@ class App extends Component {
 
   }
 
-  handleExit(room) {
+  handleExit(playerRoom) {
     //for both player and principal
-    this.setState({ room });
+    const {princRoom} = this.state;
+    let princDest = Math.floor((Math.random() * princRoom.doors.length));
+    console.log(princDest)
+    console.log('principalroom: ',princRoom.doors, princRoom.doors[princDest]);
 
+    this.setState({ playerRoom: playerRoom, princRoom:  princRoom.doors[princDest] });
   }
 
   handlePickup(item) {
-    const { playerRoom, player } = this.state;
+    const { playerRoom, player, princRoom } = this.state;
     const index = playerRoom.items.indexOf(item);
     if (index > -1) playerRoom.items.splice(index, 1);
-
     player.inventory.push(item);
+
+    let princDestIndex = Math.floor((Math.random() * princRoom.doors.length));
+    let princDest = princRoom.doors[princDestIndex];
+
     this.setState({
-      playerRoom, player
+      playerRoom, player, princRoom:  princDest
     });
+
   }
 
 
@@ -46,18 +54,18 @@ class App extends Component {
 
   render() {
     // console.log(player)
-    const { player, principal, playerRoom, principalRoom } = this.state;
+    const { player, playerRoom } = this.state;
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h6>{player.name}</h6>
-          <h6>{player.inventory.join(', ')}</h6>
-        </div>
-        <Room room={playerRoom}
-          onExit={this.handleExit}
-          onPickup={this.handlePickup}
-        />
+      <div className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h6>{player.name}</h6>
+      <h6>{player.inventory.join(', ')}</h6>
+      </div>
+      <Room room={playerRoom}
+      onExit={this.handleExit}
+      onPickup={this.handlePickup}
+      />
       </div>
     );
   }
