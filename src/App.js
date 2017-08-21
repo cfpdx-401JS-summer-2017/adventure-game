140c464b3e7292657b5d0e4996011d1d1d17106d
Anwar Montasir <anwarmontasir@gmail.com>
2017-08-21 13:54:24 -0700

    introductory HTML and CSS work

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import player from './components/player';
+import rooms from './components/rooms';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      player,
+      rooms
+    }
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+      <div id="app">
+        <p>
+          You are standing in a maze.
         </p>
       </div>
     );
