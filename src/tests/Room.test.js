cf877c036b2b2543b948a7c354be008fb896cc20
Stephanie Fitzgerald <s.e.v.fitzgerald@gmail.com>
2017-08-21 11:59:59 -0700

    Room door count test passing

--- src/tests/Room.test.js
+++ src/tests/Room.test.js
@@ -2,12 +2,18 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import toJSON from 'enzyme-to-json';
 
-import { rooms } from '../modules/Room';
+import Room from '../modules/Room';
+import rooms from '../modules/rooms';
 
-xdescribe('Room tests', () => {
+describe('Room tests', () => {
 
   it('each room has at least one door', () => {
-		const room = shallow(<Room )
+    // rooms.forEach(room => {
+    const wrapper = shallow(<Room room={rooms[0]} onExit={null} onPickup={null}/> );
+    const children = toJSON(wrapper.children());
+    const roomsArr = Object.values(children).filter(room => room.type === 'button');
+    expect(roomsArr.length).toBeGreaterThan(0);
+    // });
   });
 
 });
