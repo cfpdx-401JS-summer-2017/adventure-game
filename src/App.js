5e38f66cfa635cc41f6517462aa3958364413ad9
Anwar Montasir <anwarmontasir@gmail.com>
2017-08-21 15:12:09 -0700

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
-import player from './components/player';
 import rooms from './components/rooms';
+import player from './components/player';
 
 class App extends Component {
   constructor() {
@@ -17,21 +17,29 @@ class App extends Component {
     const { room, player } = this.state;
     return (
       <div id="app">
-        <Room room={room} player={player} />
+        <p>You are standing in a maze.</p>
+        <Room room={room} />
+        <Player player={player} />
       </div>
     );
   }
 }
 
-function Room({ room, player }) {
+function Room({ room }) {
   const dirArr = room.directions;
-  const playerDir = 'You see exits ' + dirArr.slice(0, dirArr.length - 1).join(', ') + ', and ' + dirArr.slice(-1) + '.';
-  const playerItems = 'You are carrying ' + player.items + '.';
-
+  const roomDir = 'You see exits ' + dirArr.slice(0, dirArr.length - 1).join(', ') + ', and ' + dirArr.slice(-1) + '.';
   return (
     <div>
-      <p>You are standing in a maze.</p>
-      <p>{playerDir} {playerItems}</p>
+      <p>{roomDir}</p>
+    </div>
+  );
+}
+
+function Player({ player }) {
+  const playerItems = 'You are carrying ' + player.items + '.';
+  return (
+    <div>
+      <p>{playerItems}</p>
       <p>&gt; <input ref={input => input && input.focus()} type="text" name="gameInput" className="gameInput" /></p>
     </div>
   );
