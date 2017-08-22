6f4386f8269ee3b57b5ed7faf20f189d5b7bf42a
Wendy Beck <webeck@gmail.com>
2017-08-22 09:20:18 -0700

    merging to master

--- src/modules/Challenge.js
+++ src/modules/Challenge.js
@@ -11,12 +11,32 @@ export default function Challenge(challenge) {
   return (
     <div>
       <div className="question" name="challengeQuestion" value="challengeQuestion">{challenge.question}</div>
+
       <div className="answer" name="challengeAnswer" value="challengeAnswer">{challenge.answer}</div>
+      {/* <div className="radio">
+          <label>
+            <input type="radio" value="option1" checked={true} />
+            {/* {challenge} */}
+          {/* </label> */}
+        {/* </div> */}
+        {/* <div className="radio"> */}
+          {/* <label>
+            <input type="radio" value="option2" />
+            Option 2
+          </label> */}
+        {/* </div>
+        <div className="radio">
+          <label>
+            <input type="radio" value="option3" />
+            Option 3
+          </label>
+        </div> */}
+
+      {/* </div> */}
 
 
 
-
-    </div>
+     </div>
 
   );
 }
